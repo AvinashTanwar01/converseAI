53de1df3e77a4ed76de4a6fb601783eaeb28f74c
Avinash <tanwaravinash352@gmail.com>
2025-03-25 01:43:20 +0530

    final Version

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,9 +5,10 @@ import { Button } from './ui/button';
 import Link from 'next/link';
 import { getRandomInterviewCover } from '@/lib/utils';
 import DisplayTechIcons from './DisplayTechIcons';
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
 
-const InterviewCard = ({id,role,userId,type,techstack,createdAt}: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+const InterviewCard =async  ({id,role,userId,type,techstack,createdAt}: InterviewCardProps) => {
+    const feedback = userId && id ? await getFeedbackByInterviewId({interviewId: id, userId}) : null;
     const normalizedType= /mix/gi.test(type) ? "Mixed" : type;
     const formattedDate= dayjs(feedback?.createdAt || createdAt ||Date.now()).format("MMM D,YYYY");
   return (
